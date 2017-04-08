af75df0774961454ed91b5ef8262cae06e3168de
Matt Dickinson <matt@mdickin.com>
2017-04-08 15:51:33 -0500

    Declare exports inline

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,24 +1,22 @@
 import * as _ from 'underscore';
 
-export { getParameters, isValueType, objectByString, assignObjectByString, getItemCount }
-
 const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
 const ARGUMENT_NAMES = /([^\s,]+)/g;
-function getParameters(func): string[] {
+export function getParameters(func): string[] {
     const noComments = func.toString().replace(STRIP_COMMENTS, '');
     const result = noComments.slice(noComments.indexOf('(') + 1, noComments.indexOf(')')).match(ARGUMENT_NAMES);
 
     return result === null ? [] : result;
 }
 
-function isValueType(s) {
+export function isValueType(s) {
     return _.isNumber(s) ||
         _.isString(s) ||
         _.isBoolean(s) ||
         _.isDate(s);
 }
 
-function objectByString<T>(obj: Object, path: string): T {
+export function objectByString<T>(obj: Object, path: string): T {
     path = path.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
     if (path[0] == '.') { path = path.substr(1); }// strip a leading dot
     const parts = path.split('.');
@@ -33,7 +31,7 @@ function objectByString<T>(obj: Object, path: string): T {
     return <T>obj;
 }
 
-function assignObjectByString(obj: Object, path: string, value: any) {
+export function assignObjectByString(obj: Object, path: string, value: any) {
     path = path.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
     if (path[0] == '.') { path = path.substr(1); }// strip a leading dot
     const parts = path.split('.');
@@ -49,7 +47,7 @@ function assignObjectByString(obj: Object, path: string, value: any) {
 }
 
 
-function getItemCount(obj: any[] | Object) {
+export function getItemCount(obj: any[] | Object) {
     if (_.isArray(obj)) {
         return (<any[]>obj).length;
     }
