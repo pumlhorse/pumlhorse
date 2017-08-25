5b61bf4dae5f398c383547da1b16f9bf3ca99e8d
Matt Dickinson <matt@mdickin.com>
2017-08-24 20:27:54 -0500

    Fix line endings (for real this time?)

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,7 @@ const cli = require("../lib/cli/cli")
 
 cli.run(process.argv)
     .then(function (isSuccess) { 
-        process.exit(isSuccess ? 0 : 1) 
+        process.exit(isSuccess ? 0 : 1)
     })
     .catch(function () {
         process.exit(1);
